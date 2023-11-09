67e0a6bbc230e9348a10f170c8a295ccd68d8afd
integgy <matthewregidor@gmail.com>
2023-11-08 22:15:18 -0800

    Setup Webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector("canvas")
+const c = canvas.getContext("2d")
+const cTwo = canvas.getContext("2d")
+
+
+console.log("Webpack is running :)")
+
+
+c.fillRect(50, 50, 50, 50)
+
